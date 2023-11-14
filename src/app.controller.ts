e1f6682d2813829d9f4ed4af4b5562b919e1fb9f
hakuunabatata <emachadomartins1999@gmail.com>
2023-11-13 22:27:42 -0300

    chore: add eslint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,12 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get } from '@nestjs/common'
+import { AppService } from './app.service'
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-
   getHello(): { message: string } {
-    return this.appService.getHello();
+    return this.appService.getHello()
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
 
 const { PORT = '5000' } = process.env
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule)
   console.log(process.env)
-  await app.listen(+PORT);
+  await app.listen(+PORT)
 }
 
-bootstrap();
+bootstrap()
